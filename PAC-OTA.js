b832b948e0565dc8b65a2f1a9b8dea6b0504ed2b
kienqb94 <56856970+kienqb94@users.noreply.github.com>
2020-06-12 14:28:10 +0700

    Update PAC-OTA.js

--- PAC-OTA.js
+++ PAC-OTA.js
@@ -1,6 +1,6 @@
 var DIRECT = "DIRECT";
 var PROXY = "PROXY 127.0.0.1:80";
-var blacklist = {"appldnld.apple.com":1,"mesu.apple.com":1,"gdmf.apple.com":1,"ocsp.apple.com":1};
+var blacklist = {"ioslm.haoictdev.com":1,"appldnld.apple.com":1,"mesu.apple.com":1,"gdmf.apple.com":1,"ocsp.apple.com":1};
 function FindProxyForURL(url, host) {
   host = host.toLowerCase();
   for (i = 0; i < 30; i++) {
